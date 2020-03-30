f3553d113c0c316997af0e18224ca674e0eb02e9
SindhuraPaleru <51043047+SindhuraPaleru@users.noreply.github.com>
2020-03-30 23:35:23 +0530

    Create Challenge1_Solution.py

---
+++ Challenge #1 [TCS] - Suppandi & Areas/Challenge1_Solution.py
@@ -0,0 +1,15 @@
+n=int(input())
+for x in range(n):
+    shape=input()
+    if(shape=='triangle'):
+        print(int(input())*int(input()))
+    elif(shape=='rectangle'):
+        print(int(input())*int(input()))
+    elif(shape=='square'):
+        print(int(0.5*(int(input()))*(int(input()))))
+    else:
+        print(0)
+
+
+
+
