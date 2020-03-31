ec1e0e0719bcb8bcbfd2ae8c888c2792b23061db
SindhuraPaleru <51043047+SindhuraPaleru@users.noreply.github.com>
2020-03-31 23:35:55 +0530

    Create Challenge2_Solution.java

---
+++ Challenge #2 [WIPRO] - Chhota Bheem's Password/Challenge2_Solution.java
@@ -0,0 +1,48 @@
+import java.lang.*;
+import java.util.*;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner ip=new Scanner(System.in);
+        int i=ip.nextInt();
+        int k;
+        int n;
+        int a[];
+        for(int j=0;j<i;j++)
+        {
+            n=ip.nextInt();
+            a=new int[n];
+            for(k=0;k<n;k++)
+            {
+                a[k]=ip.nextInt();
+            }
+            for(k=0;k<n;k++)
+            {
+                if(a[k]==-1)
+                {
+                    if(Math.abs(a[k-1]-a[k+1])%2==0)
+                    {
+                        a[k]=Math.abs(a[k-1]-a[k+1]);
+                    }
+                    else
+                    {
+                        a[k]=(int)Math.floor((a[k-1]+a[k+1])/2);
+                    }
+                }
+            }
+            for(k=0;k<n;k++)
+            {
+                if(a[k]!=1&&k!=n-1)
+                {
+                    a[k]=a[k]-1;
+                }
+                System.out.print(a[k]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
